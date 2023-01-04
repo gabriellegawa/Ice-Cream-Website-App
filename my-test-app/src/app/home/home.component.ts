768ecee0a94e48cd466ac19c9904cbcdcb94f188
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2023-01-04 00:35:11 -0500

    added router outlet to replace url in home component html

--- my-test-app/src/app/home/home.component.ts
+++ my-test-app/src/app/home/home.component.ts
@@ -8,23 +8,10 @@ import { Router } from '@angular/router'
 })
 
 export class HomeComponent implements OnInit {
-  
-
-  allRoutes:String[] = ['/', '/service-gallery', '/product-gallery', '/user-gallery', '/user-login-form', '/user-logout', '/user-gallery']
-  exists:boolean = false
 
   constructor(public router:Router) { }
 
   ngOnInit(): void {
-  }
-
-  routeExists() {
-    for (let route of this.allRoutes) {
-      if (this.router.url == route) {
-        return true
-      }
-    }
-    return false
   }
 
 }
