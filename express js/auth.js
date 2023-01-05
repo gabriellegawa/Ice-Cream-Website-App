4abe29e69e405b7a6355903fc16ddfe8d4355018
gabriellegawa <gabriellegawa@gmail.com>
2023-01-04 20:16:08 -0500

    first init back end restructure

---
+++ express js/auth.js
@@ -0,0 +1,14 @@
+// const fs = require("fs")
+// const jwt = require("jsonwebtoken")
+
+// const RSA_PRIVATE_KEY = fs.readFileSync("./demos/private.key")
+
+// function generateToken(userId) {
+//     return jwt.sign({}, RSA_PRIVATE_KEY, {
+//         algorithm: 'HS256',
+//         expiresIn: 120,
+//         subject: userId
+//     })
+// }
+
+// module.exports = {generateToken}
