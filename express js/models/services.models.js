1951348f46aaa6efbb63a1f8f40bb9cc8b8171fd
gabriellegawa <gabriellegawa@gmail.com>
2023-01-13 22:57:31 -0500

    add validator for service model

--- express js/models/services.models.js
+++ express js/models/services.models.js
@@ -1,28 +1,38 @@
 const mongoose = require("mongoose");
-var validate = require('mongoose-validator');
-
-var titleValidator = [
-    validate({
-      validator: 'isLength',
-      arguments: [15, 50],
-      message: 'Name should be between {ARGS[0]} and {ARGS[1]} characters',
-    })
-];
-  
-
 var objectId = require('mongoose').Types.ObjectId; 
+
+var userAccountModel = require('./userAccount.models')
 
 mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://localhost:27017/iCreamDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const servicesSchema = mongoose.Schema({
-    title: { type: String, required: true, validate: titleValidator },
-    description: String,
-    dateAdded: Date,
-    lastUpdated: String,
+    title: { 
+        type: String, 
+        required: true ,
+        minlength: 5,
+        maxlength: 25,
+        },
+    description: { 
+        type: String, 
+        required: true,
+        maxlength: 75 },
+    dateAdded: {type: Date, required: true },
+    lastUpdated: {type: Date, required: true },
     userAccount: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "userAccount"
+        ref: "userAccount",
+        required: true,
+        validate: {
+            async validator(val) {
+                const result = await userAccountModel.findOne({
+                    "_id": val
+                })
+                return result != null ? true : false
+            },
+            message: "userAccount doesn't exists"
+        }
+
     }
  });
 
