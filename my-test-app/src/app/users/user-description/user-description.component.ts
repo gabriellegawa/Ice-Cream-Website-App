32da96ebac91ac6a539dd62272e646f1f295bff0
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2023-02-06 23:17:35 -0500

    Improved data table column headers

--- my-test-app/src/app/users/user-description/user-description.component.ts
+++ my-test-app/src/app/users/user-description/user-description.component.ts
@@ -21,8 +21,9 @@ export class UserDescriptionComponent implements OnInit, AfterViewInit, OnDestro
   @ViewChild(MatPaginator) paginator !: MatPaginator;
   @ViewChild(MatSort) sort !: MatSort;
 
-  public displayedColumns: string[] = ['firstName', 'lastName', 'emailAddress', 'phoneNumber', 'dateOfBirth'];
-  public columnsToDisplay: string[] = [...this.displayedColumns, 'Actions'];
+  public displayedColumns: string[] = ['First Name', 'Last Name', 'Email Address', 'Phone Number', 'Date Of Birth'];
+  public userData: string[] = ['firstName', 'lastName', 'emailAddress', 'phoneNumber', 'dateOfBirth'];
+  public columnsToDisplay: string[] = [...this.userData, 'Actions'];
   public dataSource: MatTableDataSource<User>;
   private serviceSubscribe !: Subscription;
   public columnsFilters = {};
