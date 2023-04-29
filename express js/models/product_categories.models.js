a8ed3210ac6ab850130422e84203d489853a106d
gabriellegawa <gabriellegawa@gmail.com>
2023-04-29 15:35:10 -0400

    remove blank lines in product categories model

--- express js/models/product_categories.models.js
+++ express js/models/product_categories.models.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-var objectId = require('mongoose').Types.ObjectId; 
 
 mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://localhost:27017/iCreamDB", { useNewUrlParser: true, useUnifiedTopology: true });
@@ -11,6 +10,5 @@ const productCategoriesSchema = mongoose.Schema({
     lastUpdated: Date,
     dateDeleted: Date,
 });
-
 
 module.exports = mongoose.model("ProductCategories", productCategoriesSchema);
