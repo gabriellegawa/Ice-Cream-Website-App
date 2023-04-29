129b19bf09f672350abd67786cb124ce1cf65487
gabriellegawa <gabriellegawa@gmail.com>
2023-04-29 15:33:14 -0400

    add discount model

---
+++ express js/models/discounts.models.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+var objectId = require('mongoose').Types.ObjectId; 
+
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://localhost:27017/iCreamDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const discountsSchema = mongoose.Schema({
+    name: String,
+    description: String,
+    discountType: String,
+    discountValue: Number,
+    couponCode: String,
+    active: Boolean,
+    dateAdded: Date,
+    lastUpdated: Date,
+    dateDeleted: Date,
+});
+
+
+module.exports = mongoose.model("Discounts", discountsSchema);
