16c24843f5cdb8f52dff50f42aeb0691f19e7b72
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2023-06-14 00:44:49 -0400

    Added phone number validation for create user form

--- my-test-app/src/app/models/user.ts
+++ my-test-app/src/app/models/user.ts
@@ -1,25 +1,24 @@
 import { Role } from "./role"
 
-export class User{
-    _id : number
-    firstName: string
-    lastName: string
-    emailAddress: string
-    phoneNumber: number
-    dateOfBirth: string
-    password: string
-    role: Role
+export class User {
+  _id: number
+  firstName: string
+  lastName: string
+  emailAddress: string
+  phoneNumber: number
+  dateOfBirth: string
+  password: string
+  role: Role
 
-    constructor(obj?:User){
-      
-      this._id = obj?._id ?? 0
-      this.firstName = obj?.firstName ?? ''
-      this.lastName = obj?.lastName ?? ''
-      this.emailAddress = obj?.emailAddress ?? ''
-      this.phoneNumber = obj?.phoneNumber ?? 0
-      this.dateOfBirth = obj?.dateOfBirth ?? ''
-      this.password = obj?.password ?? ''
-      this.role = obj?.role ?? Role.Customer
-    }
-    
+  constructor(obj?: User) {
+    this._id = obj?._id ?? 0
+    this.firstName = obj?.firstName ?? ''
+    this.lastName = obj?.lastName ?? ''
+    this.emailAddress = obj?.emailAddress ?? ''
+    this.phoneNumber = obj?.phoneNumber ?? 0
+    this.dateOfBirth = obj?.dateOfBirth ?? ''
+    this.password = obj?.password ?? ''
+    this.role = obj?.role ?? Role.Customer
+  }
+
 }
--- my-test-app/src/app/users/create-user-form/create-user-form.component.spec.ts
+++ my-test-app/src/app/users/create-user-form/create-user-form.component.spec.ts
@@ -8,6 +8,23 @@ describe('CreateUserFormComponent', () => {
   let component: CreateUserFormComponent;
   let fixture: ComponentFixture<CreateUserFormComponent>;
 
+
+  const setAllFormControlValues = () => {
+    component.createUserForm.controls['veh_firstName'].setValue('John');
+    component.createUserForm.controls['veh_lastName'].setValue('Doe');
+    component.createUserForm.controls['veh_emailAddress'].setValue('john.doe@gmail.com');
+    component.createUserForm.controls['veh_phoneNumber'].setValue('1234567890');
+    component.createUserForm.controls['veh_dateOfBirth'].setValue('01/01/2000');
+    component.createUserForm.controls['veh_password'].setValue('password');
+    component.createUserForm.controls['veh_confirmPassword'].setValue('password');
+
+    fixture.detectChanges();
+  }
+
+  const setFormControlValue = (formControlName: string, value: string) => {
+    component.createUserForm.controls[formControlName].setValue(value);
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
@@ -43,5 +60,75 @@ describe('CreateUserFormComponent', () => {
   it('should have a button with text "Cancel"', () => {
     let button = fixture.debugElement.nativeElement.querySelectorAll("button").item(1).textContent
     expect(button).toEqual('Cancel');
+  })
+
+  it('should have a button with text "Create" and be initially disabled', () => {
+    let button = fixture.debugElement.nativeElement.querySelectorAll("button").item(0)
+    expect(button.disabled).toBeTruthy();
+  })
+
+  it('should enable button when all fields are filled', () => {
+
+    setAllFormControlValues();
+
+    let button = fixture.debugElement.nativeElement.querySelectorAll("button").item(0)
+    expect(button.disabled).toBeFalsy();
+  })
+
+  it('should disable button when all fields are filled except first name', () => {
+    setFormControlValue('veh_lastName', 'Doe');
+    setFormControlValue('veh_emailAddress', 'john.doe@gmail.com');
+    setFormControlValue('veh_phoneNumber', '1234567890');
+    setFormControlValue('veh_dateOfBirth', '01/01/2000');
+    setFormControlValue('veh_password', 'password');
+    setFormControlValue('veh_confirmPassword', 'password');
+
+    fixture.detectChanges();
+
+    let button = fixture.debugElement.nativeElement.querySelectorAll("button").item(0)
+    expect(button.disabled).toBeTruthy();
+  })
+
+  it('should disable button when all fields are filled except last name', () => {
+    setFormControlValue('veh_firstName', 'John');
+    setFormControlValue('veh_emailAddress', 'john.doe@gmail.com');
+    setFormControlValue('veh_phoneNumber', '1234567890');
+    setFormControlValue('veh_dateOfBirth', '01/01/2000');
+    setFormControlValue('veh_password', 'password');
+    setFormControlValue('veh_confirmPassword', 'password');
+
+
+    fixture.detectChanges();
+
+    let button = fixture.debugElement.nativeElement.querySelectorAll("button").item(0)
+    expect(button.disabled).toBeTruthy();
+  })
+
+  it('should disable button when all fields are filled except email address', () => {
+    setFormControlValue('veh_firstName', 'John');
+    setFormControlValue('veh_lastName', 'Doe');
+    setFormControlValue('veh_phoneNumber', '1234567890');
+    setFormControlValue('veh_dateOfBirth', '01/01/2000');
+    setFormControlValue('veh_password', 'password');
+    setFormControlValue('veh_confirmPassword', 'password');
+
+    fixture.detectChanges();
+
+    let button = fixture.debugElement.nativeElement.querySelectorAll("button").item(0)
+    expect(button.disabled).toBeTruthy();
+  })
+
+  it('should disable button when all fields are filled except phone number', () => {
+    setFormControlValue('veh_firstName', 'John');
+    setFormControlValue('veh_lastName', 'Doe');
+    setFormControlValue('veh_emailAddress', 'john.doe@gmail.com');
+    setFormControlValue('veh_dateOfBirth', '01/01/2000');
+    setFormControlValue('veh_password', 'password');
+    setFormControlValue('veh_confirmPassword', 'password');
+
+    fixture.detectChanges();
+
+    let button = fixture.debugElement.nativeElement.querySelectorAll("button").item(0)
+    expect(button.disabled).toBeTruthy();
   })
 });
